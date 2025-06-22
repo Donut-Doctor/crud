67dae685e22e79112c490cf835e21ce3de01b048
Donut-Doctor <palsuman174@gmail.com>
2025-06-22 17:07:02 +0530

    slight changes in interface

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -2,20 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Item = require("../models/Item");
 
-// GET all items
 router.get("/", async (req, res) => {
   const items = await Item.find();
   res.json(items);
 });
 
-// POST a new item
 router.post("/", async (req, res) => {
   const newItem = new Item({ name: req.body.name });
   await newItem.save();
   res.json(newItem);
 });
 
-// DELETE an item
 router.delete("/:id", async (req, res) => {
   await Item.findByIdAndDelete(req.params.id);
   res.json({ success: true });
